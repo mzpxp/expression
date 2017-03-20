60414f88124eeec5817802b8e226355a886c7c06
Mizuki Hidaka <mzkhdk_02@yahoo.co.jp>
2017-03-20 14:41:07 +0000

    like

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -21,7 +21,7 @@ class MicropostsController < ApplicationController
     end
   end
   
-    private
+      private
   def micropost_params
     params.require(:micropost).permit(:content, :tag)
 
--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,9 +2,9 @@ class Micropost < ActiveRecord::Base
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
+  
   has_many :likes, dependent: :destroy
   def like_user(user_id)
    likes.find_by(user_id: user_id)
   end
-
 end
